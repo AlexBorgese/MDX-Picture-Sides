cc78e9569201758889c2fa35f4a6ce12ddc72993
Alex.Borgese <you@example.com>
2020-05-16 16:23:42 +0100

    feat(slides): adding styling and animations

--- slide.js
+++ slide.js
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+import posed, { PoseGroup } from "react-pose";
+
 export default ({ imageSrc, answer }) => {
   const [showAnswer, toggle] = useState(false);
+  const [isLoading, toggleloading] = useState(true);
 
   return (
-    <Slide>
-      <Image src={imageSrc} />
-      <Button onClick={() => toggle(!showAnswer)}>
-        {showAnswer ? `${answer.month} ${answer.year}` : "Show Answer"}
-      </Button>
-    </Slide>
+    <PoseGroup>
+      <Slide key={answer.month}>
+        {isLoading && <Loading>Loading</Loading>}
+        <Image
+          src={imageSrc}
+          loading="lazy"
+          alt={answer.month}
+          onLoad={() => toggleloading(!isLoading)}
+        />
+        <Button onClick={() => toggle(!showAnswer)}>
+          {showAnswer ? `${answer.month} ${answer.year}` : "Show Answer"}
+        </Button>
+      </Slide>
+    </PoseGroup>
   );
 };
 
-const Slide = styled.div`
+const PosedSlide = posed.div({
+  enter: {
+    y: 0,
+    opacity: 1,
+    delay: 300,
+    transition: {
+      y: { type: "spring", stiffness: 1000, damping: 15 },
+      default: { duration: 300 }
+    }
+  },
+  exit: {
+    y: 50,
+    opacity: 0,
+    transition: { duration: 150 }
+  }
+});
+
+const PosedText = posed.div({
+  exit: {
+    y: 50,
+    opacity: 0,
+    transition: { duration: 150 }
+  }
+});
+
+const Slide = styled(PosedSlide)`
   width: "100vw";
   height: "100vh";
   backgroundcolor: "tomato";
@@ -32,4 +68,11 @@ const Image = styled.img`
 
 const Button = styled.button`
   font-size: 50px;
+`;
+
+const Loading = styled(PosedText)`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 800px;
 `;
