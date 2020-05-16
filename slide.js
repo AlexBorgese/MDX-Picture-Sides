c1db81495d9460a9d0cec5c069978302ca840d17
Alex.Borgese <you@example.com>
2020-05-16 15:38:10 +0100

    feat(slides): add data and working button

--- slide.js
+++ slide.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 export default ({ imageSrc, answer }) => {
   const [showAnswer, toggle] = useState(false);
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "tomato"
-      }}
-    >
-      <img src={imageSrc} />
-      <button onClick={() => toggle(!showAnswer)}>answer</button>
-      {showAnswer && answer}
-    </div>
+    <Slide>
+      <Image src={imageSrc} />
+      <Button onClick={() => toggle(!showAnswer)}>
+        {showAnswer ? `${answer.month} ${answer.year}` : "Show Answer"}
+      </Button>
+    </Slide>
   );
 };
+
+const Slide = styled.div`
+  width: "100vw";
+  height: "100vh";
+  backgroundcolor: "tomato";
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  object-fit: cover;
+  //   max-width: 500px;
+  max-height: 800px;
+  width: auto;
+  height: auto;
+`;
+
+const Button = styled.button`
+  font-size: 50px;
+`;
